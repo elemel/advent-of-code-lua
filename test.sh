ec9d921720d018a9d779773f67080f507bd05333
Mikael Lind <elemel@elemel.se>
2019-12-01 21:26:08 +0100

    Spaces not tabs

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
 for day in 2018/12/* 2019/12/*
 do
-	if lua $day/part_1.lua < $day/input.txt | diff $day/answer_1.txt -
-	then
-		tput setaf 2
-		echo $day/part_1.lua: PASS
-		tput sgr0
-	else
-		tput setaf 1
-		echo $day/part_1.lua: FAIL
-		tput sgr0
-	fi
+  if lua $day/part_1.lua < $day/input.txt | diff $day/answer_1.txt -
+  then
+    tput setaf 2
+    echo $day/part_1.lua: PASS
+    tput sgr0
+  else
+    tput setaf 1
+    echo $day/part_1.lua: FAIL
+    tput sgr0
+  fi
 
-	if lua $day/part_2.lua < $day/input.txt | diff $day/answer_2.txt -
-	then
-		tput setaf 2
-		echo $day/part_2.lua: PASS
-		tput sgr0
-	else
-		tput setaf 1
-		echo $day/part_2.lua: FAIL
-		tput sgr0
-	fi
+  if lua $day/part_2.lua < $day/input.txt | diff $day/answer_2.txt -
+  then
+    tput setaf 2
+    echo $day/part_2.lua: PASS
+    tput sgr0
+  else
+    tput setaf 1
+    echo $day/part_2.lua: FAIL
+    tput sgr0
+  fi
 done
