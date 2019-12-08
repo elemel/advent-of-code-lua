011d7604d4061623823893589c0ac07e714d93e6
Mikael Lind <elemel@elemel.se>
2019-12-08 09:21:08 +0100

    Remove unused variables

--- 2019/12/05/part_2.lua
+++ 2019/12/05/part_2.lua
@@ -1,10 +1,4 @@
 local intcode = require("intcode")
-local yulea = require("yulea")
-
-local array = yulea.array
-local elements = yulea.elements
-local map = yulea.map
-local split = yulea.split
 
 local program = intcode.compile(io.read())
 program.inputs:push_right(5)
