5acc69b0665060a0e65fc869b79b117ffbe71cdb
Mikael Lind <elemel@elemel.se>
2019-12-08 09:14:22 +0100

    Use intcode module in 2019-12-02

--- 2019/12/02/part_1.lua
+++ 2019/12/02/part_1.lua
@@ -1,36 +1,9 @@
-local yulea = require("yulea")
+local intcode = require("intcode")
 
-local array = yulea.table.array
-local map = yulea.iterator.map
-local split = yulea.string.split
-
-local program = array(
-  map(
-    split(io.read(), ","),
-    tonumber),
-  {}, 0)
+local program = intcode.compile(io.read())
 
 program[1] = 12
 program[2] = 2
 
-local ip = 0
-
-while program[ip] ~= 99 do
-  if program[ip] == 1 then
-    local a = program[ip + 1]
-    local b = program[ip + 2]
-    local c = program[ip + 3]
-
-    program[c] = program[a] + program[b]
-    ip = ip + 4
-  elseif program[ip] == 2 then
-    local a = program[ip + 1]
-    local b = program[ip + 2]
-    local c = program[ip + 3]
-
-    program[c] = program[a] * program[b]
-    ip = ip + 4
-  end
-end
-
+intcode.run(program)
 print(program[0])
--- 2019/12/02/part_2.lua
+++ 2019/12/02/part_2.lua
@@ -1,54 +1,20 @@
-local yulea = require("yulea")
+local intcode = require("intcode")
 
-local array = yulea.table.array
-local copy = yulea.table.copy
-local elements = yulea.table.elements
-local map = yulea.iterator.map
-local split = yulea.string.split
+local function run(source, noun, verb)
+  local program = intcode.compile(source)
 
-local function run(program, noun, verb)
-  local memory = copy(program)
+  program[1] = noun
+  program[2] = verb
 
-  memory[1] = noun
-  memory[2] = verb
-
-  local ip = 0
-
-  while true do
-    local opcode = memory[ip]
-
-    if opcode == 99 then
-      return memory[0]
-    end
-
-    if opcode == 1 then
-      local a = memory[ip + 1]
-      local b = memory[ip + 2]
-      local c = memory[ip + 3]
-
-      memory[c] = memory[a] + memory[b]
-      ip = ip + 4
-    elseif opcode == 2 then
-      local a = memory[ip + 1]
-      local b = memory[ip + 2]
-      local c = memory[ip + 3]
-
-      memory[c] = memory[a] * memory[b]
-      ip = ip + 4
-    end
-  end
+  intcode.run(program)
+  return program[0]
 end
 
-local program = array(
-  map(
-    split(io.read(), ","),
-    tonumber),
-  {},
-  0)
+local source = io.read()
 
 for noun = 0, 99 do
   for verb = 0, 99 do
-    local result = run(program, noun, verb)
+    local result = run(source, noun, verb)
 
     if result == 19690720 then
       print(100 * noun + verb)
