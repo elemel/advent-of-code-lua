f97407a988a0fc2aa132a08b491d07d8f907e09d
Mikael Lind <elemel@elemel.se>
2019-12-10 11:19:18 +0100

    Rank directions using rational math only

--- 2019/12/10/part_2.lua
+++ 2019/12/10/part_2.lua
@@ -62,14 +62,61 @@ for dy, row in pairs(groups) do
   end
 end
 
-function angle(directionPositions)
-  local direction, _ = table.unpack(directionPositions)
-  local dx, dy = table.unpack(direction)
-  return math.atan2(dx, -dy) % (2 * math.pi)
+function rankDirection(x, y)
+  if x < 0 then
+    if y < 0 then
+      -- Southwest
+      return 5, -y, -x
+    elseif 0 < y then
+      -- Northwest
+      return 3, -x, y
+    else
+      -- West
+      return 4, 0, -x
+    end
+  elseif 0 < x then
+    if y < 0 then
+      -- Southeast
+      return 7, x, -y
+    elseif 0 < y then
+      -- Northeast
+      return 1, y, x
+    else
+      -- East
+      return 0, 0, x
+    end
+  else
+    if y < 0 then
+      -- South
+      return 6, 0, -y
+    elseif 0 < y then
+      -- North
+      return 2, 0, y
+    else
+      error("Zero direction")
+    end
+  end
 end
 
-table.sort(sortedGroups, function(a, b)
-  return angle(a) < angle(b)
+function compareDirections(x1, y1, x2, y2)
+  local a1, b1, c1 = rankDirection(x1, y1)
+  local a2, b2, c2 = rankDirection(x2, y2)
+
+  if a1 ~= a2 then
+    return a1 < a2
+  end
+
+  return b1 * c2 < b2 * c1
+end
+
+table.sort(sortedGroups, function(directionPositions1, directionPositions2)
+  local direction1, _ = table.unpack(directionPositions1)
+  local direction2, _ = table.unpack(directionPositions2)
+
+  local dx1, dy1 = table.unpack(direction1)
+  local dx2, dy2 = table.unpack(direction2)
+
+  return compareDirections(-dy1, dx1, -dy2, dx2)
 end)
 
 local j = 1
