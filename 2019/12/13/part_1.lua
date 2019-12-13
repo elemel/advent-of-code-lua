f449c26f2193f520442ac30056a9acd94bb972ac
Mikael Lind <elemel@elemel.se>
2019-12-13 06:41:10 +0100

    Solve 2019-12-13

---
+++ 2019/12/13/part_1.lua
@@ -0,0 +1,26 @@
+local midwint = require("midwint")
+
+local program = midwint.Program.new(io.read())
+program:run()
+local screen = {}
+
+while not program.outputQueue:isEmpty() do
+  local x = program.outputQueue:pop()
+  local y = program.outputQueue:pop()
+  local id = program.outputQueue:pop()
+
+  screen[y] = screen[y] or {}
+  screen[y][x] = id
+end
+
+local count = 0
+
+for y, row in pairs(screen) do
+  for x, tile in pairs(row) do
+    if tile == 2 then
+      count = count + 1
+    end
+  end
+end
+
+print(count)
---
+++ 2019/12/13/part_2.lua
@@ -0,0 +1,65 @@
+local midwint = require("midwint")
+
+local function printScreen(screen)
+  local tiles = {"#", "%", "=", "O"}
+
+  for y, row in ipairs(screen) do
+    local line = {}
+
+    for x, tile in pairs(row) do
+      line[#line + 1] = tiles[tile] or " "
+    end
+
+    print(table.concat(line))
+  end
+end
+
+local program = midwint.Program.new(io.read())
+program[0] = 2
+
+local input = 0
+local screen = {}
+local score
+
+repeat
+  program.inputQueue:push(input)
+  program:run()
+
+  local paddleX
+  local ballX
+
+  while not program.outputQueue:isEmpty() do
+    local x = program.outputQueue:pop()
+    local y = program.outputQueue:pop()
+    local id = program.outputQueue:pop()
+
+    if x == -1 and y == 0 then
+      score = id
+    else
+      screen[y] = screen[y] or {}
+      screen[y][x] = id
+
+      if id == 3 then
+        paddleX = x
+      end
+
+      if id == 4 then
+        ballX = x
+      end
+    end
+  end
+
+  -- printScreen(screen)
+
+  input = 0
+
+  if paddleX and ballX then
+    if paddleX < ballX then
+      input = 1
+    elseif paddleX > ballX then
+      input = -1
+    end
+  end
+until program:isHalted()
+
+print(score)
