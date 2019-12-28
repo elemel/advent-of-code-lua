432e4ba180722c0d45d0e20225bc90e95029a459
Mikael Lind <elemel@elemel.se>
2019-12-28 16:41:08 +0100

    Use deque instead of Queue in yulea

--- yulea/graph.lua
+++ yulea/graph.lua
@@ -1,25 +1,25 @@
-local Queue = require("yulea.Queue")
+local deque = require("yulea.deque")
 
 local function breadthFirstSearch(neighborArrays, origin)
   local parents = {}
   parents[origin] = origin
 
-  local queue = Queue.new()
-  queue:push(origin)
+  local queue = deque.new()
+  queue:push_right(origin)
 
   repeat
-    local vertex = queue:pop()
+    local vertex = queue:pop_left()
     local neighbors = neighborArrays[vertex]
 
     if neighbors then
       for _, neighbor in ipairs(neighbors) do
         if parents[neighbor] == nil then
           parents[neighbor] = vertex
-          queue:push(neighbor)
+          queue:push_right(neighbor)
         end
       end
     end
-  until queue:isEmpty()
+  until queue:is_empty()
 
   parents[origin] = nil
   return parents
